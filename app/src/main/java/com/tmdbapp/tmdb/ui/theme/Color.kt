7202c587892ad1d1e4b300ed8f0309a6fa8560d0
Vaishak Babu <vichunew93@gmail.com>
2024-06-23 20:12:45 +0530

    commit changes

--- app/src/main/java/com/tmdbapp/tmdb/ui/theme/Color.kt
+++ app/src/main/java/com/tmdbapp/tmdb/ui/theme/Color.kt
@@ -10,5 +10,5 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val AppBackground = Color(0xFF1E1E1E)
+val AppBackground = Color(0xFF242A32)
 val TextFontColor = Color(0xFFFFFFFF)
